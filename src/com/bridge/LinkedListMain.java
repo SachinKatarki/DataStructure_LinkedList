501c480ca597ed31c9ed9942f03b4da15e8dd5a1
Textspot <sachinkatarki1995@gmail.com>
2021-11-15 14:49:25 +0530

    Added:Delete the Last Node Element Programme

--- src/com/bridge/LinkedListMain.java
+++ src/com/bridge/LinkedListMain.java
@@ -5,13 +5,14 @@ import java.util.Scanner;
 
 public class LinkedListMain {
 	
-	   public static void main(String[] args) {
+	 public static void main(String[] args) {
 	        System.out.println("Welcome to the LinkedList Code !!");
 	        Scanner scanner = new Scanner(System.in);
 	        System.out.println("Press '1' to add the data at the start");
 	        System.out.println("Press '2' to add the data at the end");
 	        System.out.println("Press '3' to insert data inBetween");
 	        System.out.println("Press '4' to delete data of first node");
+	        System.out.println("Press '5' to delete data of last node");
 	        switch (scanner.nextInt()) {
 	            case 1:
 	                Operations.addDataAtStart();
@@ -24,6 +25,9 @@ public class LinkedListMain {
 	                break;
 	            case 4:
 	                Operations.deleteFirstNode();
+	                break;
+	            case 5:
+	                Operations.deleteLastNode();
 	                break;
 	        }
 
--- src/com/bridge/Operations.java
+++ src/com/bridge/Operations.java
@@ -22,4 +22,9 @@ public class Operations {
 		
 	}
 
+	public static void deleteLastNode() {
+	
+		
+	}
+
 }
