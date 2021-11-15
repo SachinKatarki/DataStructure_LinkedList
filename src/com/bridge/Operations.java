edbd1df8bd0263bd28a28a57d67fa34a43af812a
Sachin <90496023+SachinKatarki@users.noreply.github.com>
2021-11-15 15:48:18 +0530

    Update Operations.java

--- src/com/bridge/Operations.java
+++ src/com/bridge/Operations.java
@@ -2,49 +2,78 @@ package com.bridge;
 
 public class Operations {
 
-	public static void addDataAtStart() {
-		
-		
-	}
+        public static LinkedList addDataAtStart() {
+        LinkedList linkedList = new LinkedList();
+        linkedList.push(70);
+        linkedList.push(30);
+        linkedList.push(56);
+        linkedList.print();
+        return linkedList;
+    }
 
-	public static void addDataAtEnd() {
-		
-		
-	}
+    public static void addDataAtEnd() {
+        LinkedList linkedList = new LinkedList();
+        linkedList.append(56);
+        linkedList.append(30);
+        linkedList.append(70);
+        linkedList.print();
+    }
 
-	public static void insertInBetween() {
-		
-		
-	}
+    public static void insertInBetween() {
+        LinkedList linkedList = new LinkedList();
+        Node secondNode = linkedList.push(70);
+        Node firstNode = linkedList.push(56);
+        Node newNode = new Node(30);
+        System.out.println("Before:");
+        linkedList.print();
+        linkedList.insertInBetween(firstNode, newNode);
+        System.out.println("after:");
+        linkedList.print();
 
-	public static void deleteFirstNode() {
-	
-		
-	}
+    }
 
-	public static void deleteLastNode() {
-	
-		
-	}
+    public static void deleteFirstNode() {
+        LinkedList linkedList = addDataAtStart();
+        linkedList.pop();
+        linkedList.print();
+    }
 
-	public static void findNodeBasedOnValue() {
-		
-		
-	}
+    public static void deleteLastNode() {
+        LinkedList linkedList = addDataAtStart();
+        linkedList.popLast();
+        linkedList.print();
 
-	public static void insertAfterSearchNode() {
-		
-		
-	}
+    }
 
-	public static void deleteAfterNodeAfterSearchNode() {
-		
-		
-	}
+    public static void findNodeBasedOnValue() {
+        LinkedList linkedList = addDataAtStart();
+        linkedList.searchNode(30);
+    }
 
-	public static void sortList() {
-		
-		
-	}
 
+
+    public static LinkedList insertAfterSearchNode() {
+        LinkedList linkedList = addDataAtStart();
+        Node newNode = new Node(40);
+        Node previousNode = linkedList.searchNodeAt(30, newNode);
+        System.out.println("Before");
+        linkedList.print();
+        return linkedList;
+    }
+
+    public static void deleteAfterNodeAfterSearchNode() {
+        System.out.println("Before");
+        LinkedList linkedList = insertAfterSearchNode();
+        System.out.println("after");
+        linkedList.deleteANode(40);
+        linkedList.print();
+    }
+
+    public static void sortList() {
+        System.out.println("before sorting the list");
+        LinkedList linkedList = insertAfterSearchNode();
+        linkedList.sort();
+        System.out.println("after sorting the list");
+        linkedList.print();
+    }
 }
