e5980b11ccd14810a190fcf91bf3e8d1f77db0e2
Textspot <sachinkatarki1995@gmail.com>
2021-11-15 14:38:26 +0530

    Welcome to the Ability to Delete the element List Programme

--- src/com/bridge/LinkedListMain.java
+++ src/com/bridge/LinkedListMain.java
@@ -5,12 +5,13 @@ import java.util.Scanner;
 
 public class LinkedListMain {
 	
-	 public static void main(String[] args) {
+	   public static void main(String[] args) {
 	        System.out.println("Welcome to the LinkedList Code !!");
 	        Scanner scanner = new Scanner(System.in);
 	        System.out.println("Press '1' to add the data at the start");
 	        System.out.println("Press '2' to add the data at the end");
 	        System.out.println("Press '3' to insert data inBetween");
+	        System.out.println("Press '4' to delete data of first node");
 	        switch (scanner.nextInt()) {
 	            case 1:
 	                Operations.addDataAtStart();
@@ -21,9 +22,11 @@ public class LinkedListMain {
 	            case 3:
 	                Operations.insertInBetween();
 	                break;
+	            case 4:
+	                Operations.deleteFirstNode();
+	                break;
 	        }
 
 	    }
 
 	}
-
